d6c6ba346f30092f97a6b1a824e39cced0295383
rollg <maildevcom@gmail.com>
2019-02-19 19:15:18 +0200

    - initial commit

---
+++ single-spa.config.js
@@ -0,0 +1,14 @@
+import { registerApplication, start } from 'single-spa';
+
+registerApplication('navBar', () => import('./src/navBar/navBar.app.js').then(mod => mod.navBar), () => true);
+registerApplication('home', () => import('./src/home/home.app.js'), (location) => location.pathname === "" || location.pathname === "/" || location.pathname.startsWith('/home'));
+registerApplication('angularJS', () => import('./src/angularJS/angularJS.app'), pathPrefix('/angularJS'));
+
+
+start();
+
+function pathPrefix(prefix) {
+    return function (location) {
+        return location.pathname.startsWith(`${prefix}`);
+    }
+}
