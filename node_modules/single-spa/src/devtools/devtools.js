d4a8aad2689062f9dd919e11fd697a6fcb3ec20a
rollg <maildevcom@gmail.com>
2019-02-20 10:45:40 +0200

    - .gitignore updates;

---
+++ node_modules/single-spa/src/devtools/devtools.js
@@ -0,0 +1,13 @@
+import {getRawAppData} from '../applications/apps'
+import {reroute} from '../navigation/reroute'
+import {NOT_LOADED} from '../applications/app.helpers'
+import {toLoadPromise} from '../lifecycles/load'
+import {toBootstrapPromise} from '../lifecycles/bootstrap'
+
+export default {
+  getRawAppData,
+  reroute,
+  NOT_LOADED,
+  toLoadPromise,
+  toBootstrapPromise,
+}
